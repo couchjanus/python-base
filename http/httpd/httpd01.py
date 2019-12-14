7043952c763131ba2fbf3929a20585d348de5fcd
couch Janus <couchjanus@gmail.com>
2019-12-14 13:25:57 +0200

    added httpd

---
+++ http/httpd/httpd01.py
@@ -0,0 +1,36 @@
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+
+PORT_NUMBER = 8080
+
+#This class will handles any incoming request from
+#the browser 
+class myHandler(BaseHTTPRequestHandler):
+	
+	#Handler for the GET requests
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		# Send the html message
+		self.wfile.write("Hello World !")
+		return
+
+try:
+	#Create a web server and define the handler to manage the
+	#incoming request
+	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	print 'Started httpserver on port ' , PORT_NUMBER
+	
+	#Wait forever for incoming htto requests
+	server.serve_forever()
+
+except KeyboardInterrupt:
+	print '^C received, shutting down the web server'
+	server.socket.close()
+	
+
+# Run it by typing:
+
+# debarm:~/playground/python/httpserver# python example1.py
+
+# Then open your web browser on this URL: http://fox_ip_address:8080.
